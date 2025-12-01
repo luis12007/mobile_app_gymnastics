7b6548a807c0cf6ca1840826f4cde5fc4d04aec6
luis12007 <00129020@edu.sv>
2025-12-01 02:15:18 -0600

    testing new version

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,6 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-          'module-resolver',
-          {
-              alias: {
-                  '@': './src',
-                  'assets': './assets',
-              }
-          },
-      ],
-      'react-native-reanimated/plugin'
-   ],
-  };
+module.exports = {
+  presets: ['babel-preset-expo'],
+  plugins: [
+    'react-native-reanimated/plugin',
+  ],
 };
